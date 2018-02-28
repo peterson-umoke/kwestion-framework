410fe3e8166269385b55b9853843e7d2509ee3b9
Peterson Umoke <umoke10@hotmail.com>
2018-02-28 15:43:46 +0100

    completely debugged the router .. going to add the dashboard

--- system/includes/router.php
+++ system/includes/router.php
@@ -34,56 +34,99 @@ if (isset($_SERVER['REQUEST_URI'])) {
     // get the key for the last value in the call parts
     $page['basename_key']	= array_search($page['basename'], $page['call_parts']);
 
+
     // remove the last key
     if (count($page['call_parts']) > 1) {
         unset($page['call_parts'][$page['basename_key']]);
-    }
+        // compile the folder name
+        $page['folder_names']	= implode(DS, $page['call_parts']);
+        // compile the file name
+        $page['file_name'] = $page['basename'] . '.php';
 
-    // compile the folder name
-    $page['folder_names']	= implode(DS, $page['call_parts']);
+        // get the file
+        $page['fullpath']	= $page['folder_names'] . DS . $page['file_name'];
 
-    // compile the file name
-    $page['file_name'] = $page['basename'] . '.php';
-
-    // get the file
-    $page['fullpath']	= $page['folder_names'] . DS . $page['file_name'];
-
-    // import the file making sure that no file exists as single
-    if (!empty($page['call_parts'][0])) {
-        switch ($page['call_parts'][0]) {
-            case 'admin': // checks if the folder call is admin
-                    if ($page['basename'] == 'admin') {
-                        if (file_exists(SYS_VIEWS_DIR . DS . 'admin' . DS . 'welcome.php')) {
-                            require_once SYS_VIEWS_DIR . DS . 'admin' . DS . 'welcome.php';
-                        }
+        // import the file making sure that no file exists as single
+        if (!empty($page['call_parts'][0])) {
+            switch ($page['call_parts'][0]) {
+                case 'admin': // checks if the folder call is admin
+                if ($page['basename'] == 'admin') {
+                    if (file_exists(SYS_VIEWS_DIR . DS . 'welcome.php')) {
+                        require_once SYS_VIEWS_DIR . DS . 'welcome.php';
+                    }
+                } else {
+                    if (file_exists(SYS_VIEWS_DIR . DS . $page['fullpath'])) {
+                        require_once SYS_VIEWS_DIR . DS . $page['fullpath'];
                     } else {
-                        if (file_exists(SYS_VIEWS_DIR . DS . 'admin' . DS . $page['fullpath'])) {
-                            require_once SYS_VIEWS_DIR . DS . 'admin' . DS . $page['fullpath'];
+                        require_once SYS_VIEWS_DIR . DS . 'admin' . DS . '404.php';
+                    }
+                }
+                break;
+
+                default: // if nothing is inputted just get the path name in the custom views folder
+                if (file_exists(VIEWS_DIR . DS . $page['fullpath'])) {
+                    require_once VIEWS_DIR . DS . $page['fullpath'];
+                } else {
+                    if (file_exists(VIEWS_DIR . DS . 'errors' . DS . '404.php')) {
+                        require_once VIEWS_DIR . DS . 'errors' . DS . '404.php';
+                    } else {
+                        require_once SYS_VIEWS_DIR . DS . 'errors' . DS . '404.php';
+                    }
+                }
+                    break;
+                }
+        } else {
+            if (file_exists(VIEWS_DIR . DS .  $page['default_page_name'])) {
+                require_once VIEWS_DIR . DS .  $page['default_page_name'];
+            } else {
+                die("<h1> Error Occurred!</h1>" . $page['default_page_name'] . " doesn't exist in the ". VIEWS_DIR . DS ."  directory");
+            }
+        }
+    } else {
+        // if the call is lower than 1
+        // compile the folder name
+        $page['folder_names']	= implode(DS, $page['call_parts']);
+        // compile the file name
+        $page['file_ext'] = '.php';
+
+        // get the file
+        $page['fullpath']	= $page['folder_names'] . $page['file_ext'];
+
+        // import the file making sure that no file exists as single
+        if (!empty($page['call_parts'][0])) {
+            switch ($page['call_parts'][0]) {
+                case 'admin': // checks if the folder call is admin
+                        if ($page['basename'] == 'admin') {
+                            if (file_exists(SYS_VIEWS_DIR . DS . 'admin' . DS . 'welcome.php')) {
+                                require_once SYS_VIEWS_DIR . DS . 'admin' . DS . 'welcome.php';
+                            }
                         } else {
-                            if (file_exists(SYS_VIEWS_DIR . DS . 'admin' . DS . '404.php')) {
+                            if (file_exists(SYS_VIEWS_DIR . DS . 'admin' . DS . $page['fullpath'])) {
+                                require_once SYS_VIEWS_DIR . DS . 'admin' . DS . $page['fullpath'];
+                            } else {
                                 require_once SYS_VIEWS_DIR . DS . 'admin' . DS . '404.php';
                             }
                         }
-                    }
-                break;
+                    break;
 
-            default: // if nothing is inputted just get the path name in the custom views folder
-                    if (file_exists(VIEWS_DIR . DS . $page['fullpath'])) {
-                        require_once VIEWS_DIR . DS . $page['fullpath'];
-                    } else {
-                        if (file_exists(VIEWS_DIR . DS . 'errors' . DS . '404.php')) {
-                            require_once VIEWS_DIR . DS . 'errors' . DS . '404.php';
+                default: // if nothing is inputted just get the path name in the custom views folder
+                        if (file_exists(VIEWS_DIR . DS . $page['fullpath'])) {
+                            require_once VIEWS_DIR . DS . $page['fullpath'];
                         } else {
-                            require_once SYS_VIEWS_DIR . DS . 'errors' . DS . '404.php';
+                            if (file_exists(VIEWS_DIR . DS . 'errors' . DS . '404.php')) {
+                                require_once VIEWS_DIR . DS . 'errors' . DS . '404.php';
+                            } else {
+                                require_once SYS_VIEWS_DIR . DS . 'errors' . DS . '404.php';
+                            }
                         }
-                    }
-                break;
-        }
-    } else {
-        if (file_exists(VIEWS_DIR . DS .  $page['default_page_name'])) {
-            require_once VIEWS_DIR . DS .  $page['default_page_name'];
+                    break;
+                }
         } else {
-            die("<h1> Error Occurred!</h1>" . $page['default_page_name'] . " doesn't exist in the ". VIEWS_DIR . DS ."  directory");
+            if (file_exists(VIEWS_DIR . DS .  $page['default_page_name'])) {
+                require_once VIEWS_DIR . DS .  $page['default_page_name'];
+            } else {
+                die("<h1> Error Occurred!</h1>" . $page['default_page_name'] . " doesn't exist in the ". VIEWS_DIR . DS ."  directory");
+            }
         }
     }
 }
