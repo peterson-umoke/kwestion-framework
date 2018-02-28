8a3281d9d27251205bb47a7a708812961206d0aa
Peterson Umoke <umoke10@hotmail.com>
2018-02-28 02:23:07 +0100

    added the kind of actions to be taken

--- system/includes/actions.php
+++ system/includes/actions.php
@@ -3,3 +3,47 @@
 if (!defined('SITE_URL')) {
     exit('No Direct access to page');
 }
+
+/**
+ * ===========================
+ * Initiate the maintance mode of the site
+ *
+ * @author Peterson Umoke <umoke10@hotmail.com>
+ * ===========================
+ */
+
+if (MAINTENANCE_MODE === true) {
+    require_once VIEWS_DIR . '/templates/503.php';
+
+    die();
+}
+
+/**
+ * ===========================
+ * setup the environment for the site
+ *
+ * @author Peterson Umoke <umoke10@hotmail.com>
+ * ===========================
+ */
+
+switch (ENVIRONMENT) {
+    case 'development':
+        error_reporting(-1);
+        ini_set('display_errors', 1);
+    break;
+
+    case 'testing':
+    case 'production':
+        ini_set('display_errors', 0);
+        if (version_compare(PHP_VERSION, '5.3', '>=')) {
+            error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
+        } else {
+            error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_USER_NOTICE);
+        }
+    break;
+
+    default:
+        header('HTTP/1.1 503 Service Unavailable.', true, 503);
+        echo 'The application environment is not set correctly.';
+        exit(1); // EXIT_ERROR
+}
