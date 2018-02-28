8302032490deb4c9eaf7c91c476a5cc6f444d2b4
Peterson Umoke <umoke10@hotmail.com>
2018-02-28 02:23:24 +0100

    wrote an autoloader for the custom classes to be written

--- system/includes/autoload.php
+++ system/includes/autoload.php
@@ -3,3 +3,28 @@
 if (!defined('SITE_URL')) {
     exit('No Direct access to page');
 }
+
+/**
+ * This is responsible for autoloading php classes in the system libray folder and in the user's csutom library class
+ *
+ * @author Peterson Umoke <umoke10@hotmail.com>
+ */
+
+ function load_classes($classname)
+ {
+     $folders = [
+        SYS_LIBRARY_DIR . DS . $classname . '.php',
+        LIBRAY_DIR . DS . $classname . '.php',
+     ];
+
+     for ($i = 0; $i < count($folders); $i++) {
+         $filename = $folders[$i];
+
+         if (file_exists($filename)) {
+             require_once $filename;
+         }
+     }
+ }
+
+ // call the autoload
+ spl_autoload_register('load_classes');
