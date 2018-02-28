b094f95ded6ecb10ccf3c76e48fd0d48832d77d6
Peterson Umoke <umoke10@hotmail.com>
2018-02-28 22:41:21 +0100

    updated the welcome page

--- views/welcome.php
+++ views/welcome.php
@@ -5,11 +5,25 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>Kwestion Framework</title>
+	<title>Welcome to Kwestion</title>
+	<link rel="stylesheet" href="<?php echo assets('vendors/bootstrap/dist/css/bootstrap.min.css'); ?>">
 </head>
 
 <body>
-	<h1>Welcome To Kwestion Framwork</h1>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h1 class="page-header text-center">Welcome to Kwestion</h1>
+				<p>
+					<ul>
+						<li>Its awesome</li>
+						<li>Clean Urls</li>
+						<li>BS4 Compatible</li>
+					</ul>
+				</p>
+			</div>
+		</div>
+	</div>
 </body>
 
 </html>
