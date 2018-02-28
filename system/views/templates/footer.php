cce0bc16d675b2ea3c587043b5c4f50dedfd21b0
Peterson Umoke <umoke10@hotmail.com>
2018-02-28 22:40:17 +0100

    added datatables

--- system/views/templates/footer.php
+++ system/views/templates/footer.php
@@ -1,13 +1,14 @@
 <footer class="main-footer">
 	<div class="pull-right hidden-xs">
-		<b>Version</b> 2.4.0
+		<b>Version</b> 3.0.0
 	</div>
 	<strong>Copyright &copy; 2014-2016
 		<a href="https://adminlte.io">Almsaeed Studio</a>.</strong> All rights reserved.
 </footer>
 
-<!-- jQuery 3 -->
-<script src="<?php echo assets('vendors/jquery/dist/jquery.min.js'); ?>"></script>
+</div>
+<!-- wrapper -->
+
 <!-- Bootstrap 3.3.7 -->
 <script src="<?php echo assets('vendors/bootstrap/dist/js/bootstrap.min.js'); ?>"></script>
 <!-- SlimScroll -->
@@ -16,11 +17,22 @@
 <script src="<?php echo assets('vendors/fastclick/lib/fastclick.js'); ?>"></script>
 <!-- AdminLTE App -->
 <script src="<?php echo assets('js/adminlte.min.js'); ?>"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="<?php echo assets('js/demo.js'); ?>"></script>
 <script>
 	$(document).ready(function() {
+
+		// sidebar menu
 		$('.sidebar-menu').tree()
+
+		// datatables
+		$('.cms-tables').DataTable()
+		$('.cms-tables2').DataTable({
+			'paging': true,
+			'lengthChange': false,
+			'searching': false,
+			'ordering': true,
+			'info': true,
+			'autoWidth': false
+		})
 	})
 </script>
 </body>
