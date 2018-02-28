81797175ae2967498c450c05bee2118b44f5b6bb
Peterson Umoke <umoke10@hotmail.com>
2018-02-28 01:08:33 +0100

    added a super global helper funtion

--- system/helpers/globals_helper.php
+++ system/helpers/globals_helper.php
@@ -3,3 +3,14 @@
 if (!defined('SITE_URL')) {
     exit('No Direct access to page');
 }
+
+/**
+ * Get a key - value pair from a super global
+ *
+ * e.g $_SESSION[''], $_GET[''], $_POST['']
+ */
+function superglobal_value($type, $key = "")
+{
+    if (isset($type)) {
+    }
+}
