93d94dc843ee3092b1f5244d7f9a0237341b12d3
Peterson Umoke <umoke10@hotmail.com>
2019-10-10 13:16:01 +0100

    added name and last name

--- resources/views/vendor/multiauth/layouts/app.blade.php
+++ resources/views/vendor/multiauth/layouts/app.blade.php
@@ -49,7 +49,7 @@
                         @else
                         <li class="nav-item dropdown">
                             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ auth('admin')->user()->name }} <span class="caret"></span>
+                                {{ auth::user()->first_name . " " . auth::user()->last_name }} <span class="caret"></span>
                             </a>
 
                             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
