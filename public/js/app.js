fb5e99537428b7ad07c9f4aa5822faa183cf1b3e
Peterson Umoke <umoke10@hotmail.com>
2019-10-10 19:40:13 +0100

    admin styles now created

--- public/js/app.js
+++ public/js/app.js
@@ -49476,6 +49476,17 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
+/***/ "./resources/sass/admin.scss":
+/*!***********************************!*\
+  !*** ./resources/sass/admin.scss ***!
+  \***********************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
 /***/ "./resources/sass/app.scss":
 /*!*********************************!*\
   !*** ./resources/sass/app.scss ***!
@@ -49488,13 +49499,14 @@ __webpack_require__.r(__webpack_exports__);
 /***/ }),
 
 /***/ 0:
-/*!*************************************************************!*\
-  !*** multi ./resources/js/app.js ./resources/sass/app.scss ***!
-  \*************************************************************/
+/*!*****************************************************************************************!*\
+  !*** multi ./resources/js/app.js ./resources/sass/admin.scss ./resources/sass/app.scss ***!
+  \*****************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
 __webpack_require__(/*! /var/www/html/kwestionframework/resources/js/app.js */"./resources/js/app.js");
+__webpack_require__(/*! /var/www/html/kwestionframework/resources/sass/admin.scss */"./resources/sass/admin.scss");
 module.exports = __webpack_require__(/*! /var/www/html/kwestionframework/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
