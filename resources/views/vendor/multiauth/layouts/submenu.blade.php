6a529d4ea22375deaa3a0e662efacf827900d1af
Peterson Umoke <umoke10@hotmail.com>
2019-10-12 22:32:43 +0100

    using conditional to .active links

--- resources/views/vendor/multiauth/layouts/submenu.blade.php
+++ resources/views/vendor/multiauth/layouts/submenu.blade.php
@@ -1,9 +1,10 @@
 @foreach(config("menu.admin.sidebar") as $key)
-@if(((array_key_exists('route', $key) && request()->route()->getName() == $key['route']) || (array_key_exists('url', $key) && request()->path() == $key['url'])) && array_key_exists('children', $key) && !empty($key['children']))
+{{-- @if(((array_key_exists('route', $key) && request()->route()->getName() == $key['route']) || (array_key_exists('url', $key) && request()->path() == $key['url'])) && array_key_exists('children', $key) && !empty($key['children'])) --}}
+@if(array_key_exists('children', $key) && !empty($key['children']))
 <div class="aside-navbar">
     <ul class="submenubar">
         @foreach($key['children'] as $key)
-        <li class="menu-item {{ array_key_exists('route',$key) ? request()->route()->getName() == $key['route'] ? 'active' : '' : ''}}{{ array_key_exists('url',$key) ? request()->path() == $key['url'] ? 'active' : '' : ''}}" {{array_key_exists('attr',$key) ? $key['attr'] : ""}}>
+        <li class="menu-item {{ array_key_exists('route',$key) ? request()->route()->getName() == $key['route'] ? 'active' : '' : ''}}{{ array_key_exists('url',$key) ? request()->path() == $key['url'] ? 'active' : '' : ''}}" {{$key['attr'] ?? ""}}>
             <a href="{{ (array_key_exists('route', $key) && !empty($key['route'])) ? route($key['route']) : url($key['url'] ?? '#') }}">
                 <span>{{$key['title']}}</span>
                 @if ((array_key_exists('route',$key) && request()->route()->getName() == $key['route']) || (array_key_exists('url',$key) && request()->path() == $key['url']))
