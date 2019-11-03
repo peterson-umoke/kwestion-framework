715e48faf6395a5d90ea27f4a0fb0b029b9e855c
Peterson Umoke <umoke10@hotmail.com>
2019-11-03 20:15:30 +0100

    updated the database architecture, updated the columns and added type to differentiate between the two types of roles for the application

--- modules/Admin/Database/Migrations/2019_11_02_141604_create_role_table.php
+++ modules/Admin/Database/Migrations/2019_11_02_141604_create_role_table.php
@@ -17,6 +17,7 @@ class CreateRoleTable extends Migration
             $table->bigIncrements('id');
             $table->string('name');
             $table->string('description');
+            $table->string('type')->default('admins');  // admins or users
             $table->timestamps();
         });
     }
