4093b02b303a930ddbbc31b960e8c4e385762351
Peterson Umoke <umoke10@hotmail.com>
2019-11-03 20:17:07 +0100

    added view for the application

---
+++ modules/Admin/Resources/views/admins/edit.blade.php
@@ -0,0 +1,12 @@
+@extends('admin::layouts.master')
+@if(!empty($admin))
+    @section("title","Edit Admin")
+@else
+    @section("title","Create Admin")
+@endif
+
+@section("content")
+
+    hello booboo
+
+@endsection
