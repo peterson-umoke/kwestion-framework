b3e485a3193a75ffd590510e723f2b74c260e3ff
Peterson Umoke <umoke10@hotmail.com>
2019-11-03 20:17:44 +0100

    updatd the fillables for the application

--- modules/Admin/Models/Role.php
+++ modules/Admin/Models/Role.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
-    protected $fillable = ["name", "description"];
+    protected $fillable = ["name", "description", 'type'];
 
     /**
      * The roles that belong to the user.
