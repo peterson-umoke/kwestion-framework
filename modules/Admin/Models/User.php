767941274938cd06871b2a241c3d8e635d8717ba
Peterson Umoke <umoke10@hotmail.com>
2019-11-03 20:19:25 +0100

    updatd the fillables for the application

--- modules/Admin/Models/User.php
+++ modules/Admin/Models/User.php
@@ -11,7 +11,7 @@ class User extends Authenticatable
 {
     use Notifiable;
 
-    protected $fillable = ["first_name", "last_name", "email", "password"];
+    protected $fillable = ["first_name", "last_name", "email", "password", 'username'];
     protected $hidden = [
         'password', 'remember_token',
     ];
